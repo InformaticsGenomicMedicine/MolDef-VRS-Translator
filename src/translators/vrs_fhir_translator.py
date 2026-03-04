826a62ab2d79dcb3438d8da791ca722337388f1d
SalemBajjali <salem.bajjali@gmail.com>
2026-03-04 11:53:18 -0600

    refactor: streamline imports and enhance method naming for clarity in VrsFhirAlleleTranslator

--- src/translators/vrs_fhir_translator.py
+++ src/translators/vrs_fhir_translator.py
@@ -7,23 +7,11 @@ from fhir.resources.quantity import Quantity
 from fhir.resources.reference import Reference
 from ga4gh.vrs.dataproxy import create_dataproxy
 from ga4gh.vrs.models import (
-    Allele as VrsAllele,
-)
-from ga4gh.vrs.models import (
+    Allele,
     LiteralSequenceExpression,
     SequenceLocation,
     SequenceReference,
     sequenceString,
-)
-from profiles.allele import Allele as FhirAlleleProfile
-from profiles.sequence import Sequence as FhirSequenceProfile
-from resources.moleculardefinition import (
-    MolecularDefinitionLocation,
-    MolecularDefinitionLocationSequenceLocation,
-    MolecularDefinitionLocationSequenceLocationCoordinateInterval,
-    MolecularDefinitionLocationSequenceLocationCoordinateIntervalCoordinateSystem,
-    MolecularDefinitionRepresentation,
-    MolecularDefinitionRepresentationLiteral,
 )
 
 from conventions.coordinate_systems import vrs_coordinate_interval
@@ -33,13 +21,23 @@ from conventions.refseq_identifiers import (
     translate_sequence_id,
     validate_accession,
 )
+from profiles.allele import Allele as FhirAllele
+from profiles.sequence import Sequence as FhirSequence
+from resources.moleculardefinition import (
+    MolecularDefinitionLocation,
+    MolecularDefinitionLocationSequenceLocation,
+    MolecularDefinitionLocationSequenceLocationCoordinateInterval,
+    MolecularDefinitionLocationSequenceLocationCoordinateIntervalCoordinateSystem,
+    MolecularDefinitionRepresentation,
+    MolecularDefinitionRepresentationLiteral,
+)
 from translators.validations.allele import validate_allele_profile, validate_vrs_allele
 from translators.validations.indexing import apply_indexing
 from vrs_tools.normalizer import VariantNormalizer
 
 
 class VrsFhirAlleleTranslator:
-    """Handles VRS <-> FHIR Allele conversion for 'contained' format."""
+    """Provide minimal bidirectional translation between VRS Allele and FHIR Allele Profiles."""
 
     def __init__(self, dp=None, uri: str | None = None):
         self.dp = dp or create_dataproxy(uri=uri)
@@ -48,7 +46,7 @@ class VrsFhirAlleleTranslator:
 
     ##############################################################
 
-    def _validate_and_extract_sequence_location(self, locations):
+    def _is_valid_sequence_location(self, locations):
         """Validates the `sequenceLocation` structure within the provided locations to ensure all necessary attributes are present for accurate translations.
 
         Args:
@@ -66,9 +64,9 @@ class VrsFhirAlleleTranslator:
             sequence_location: The validated sequence location object.
 
         """
-        for location in locations:
+        for loc in locations:
             # Access sequenceLocation first
-            sequence_location = location.sequenceLocation
+            sequence_location = loc.sequenceLocation
             if not sequence_location:
                 raise ValueError("Missing 'sequenceLocation' in location.")
             # Check coordinateInterval existence
@@ -101,7 +99,7 @@ class VrsFhirAlleleTranslator:
 
         return sequence_location
 
-    def _decimal_to_int_strict(self, value):
+    def _convert_decimal_to_int(self, value):
         """Validate and convert a Decimal value to an integer if possible.
 
         Args:
@@ -126,7 +124,7 @@ class VrsFhirAlleleTranslator:
             )
         return value
 
-    def _validate_vrs_sequence_string(self, sequence):
+    def _validate_sequence(self, sequence):
         """Validate the sequence to ensure it contains valid characters as per VRS rules.
 
         Args:
@@ -144,7 +142,7 @@ class VrsFhirAlleleTranslator:
             raise ValueError("Invalid sequence value")
         return sequence
 
-    def _extract_allele_state_from_representation(self, representations):
+    def _get_literal_value_for_allele_state(self, representations):
         """Retrieves the literal value associated with an `allele-state` representation, if present, from the provided list of representations.
 
         Args:
@@ -170,7 +168,7 @@ class VrsFhirAlleleTranslator:
                         "Missing `literal.value` for the `allele-state` representation."
                     )
 
-    def _extract_vrs_allele_translation_core(self, expression, dp):
+    def _extract_vrs_values(self, expression, dp):
         """Extract GA4GH ID, RefSeq ID, start, end, and sequence from a VRS Allele.
 
         Args:
@@ -189,7 +187,7 @@ class VrsFhirAlleleTranslator:
 
         return refgetAccession, start_pos, end_pos, alt_allele
 
-    def _extract_refseq_from_contained_sequence(self, expression):
+    def _validate_and_extract_code(self, expression):
         if not expression.contained:
             raise ValueError("Missing 'contained' field.")
 
@@ -212,7 +210,7 @@ class VrsFhirAlleleTranslator:
     #############################################################
 
     def translate_allele_to_vrs(self, expression, normalize=True):
-        """Converts an FHIR Allele object into a GA4GH VRS Allele object.
+        """Converts an FHIR Allele Profile object into a GA4GH VRS Allele object.
 
         Args:
             expression (Allele): A FHIR-compliant Allele containing sequence location,
@@ -229,76 +227,72 @@ class VrsFhirAlleleTranslator:
         """
         validate_allele_profile(expression)
 
-        fhir_sequence_location = self._validate_and_extract_sequence_location(
-            expression.location
-        )
-        refseq_id = self._extract_refseq_from_contained_sequence(expression)
+        location_data = self._is_valid_sequence_location(expression.location)
 
-        coordinate_interval = fhir_sequence_location.coordinateInterval
-        start_value = coordinate_interval.startQuantity.value
-        end_value = coordinate_interval.endQuantity.value
-        coding_list = coordinate_interval.coordinateSystem.system.coding
+        values_needed = {
+            "refseq": self._validate_and_extract_code(expression),
+            "start": location_data.coordinateInterval.startQuantity.value,
+            "end": location_data.coordinateInterval.endQuantity.value,
+        }
 
+        coding_list = location_data.coordinateInterval.coordinateSystem.system.coding
         if len(coding_list) != 1:
             raise ValueError("Only one coding supported in coordinateSystem.")
 
-        coordinate_system_display = coding_list[0].display
+        values_needed["coordinate_system_display"] = coding_list[0].display
 
-        allele_state_literal = self._extract_allele_state_from_representation(
-            expression.representation
+        seq = self._get_literal_value_for_allele_state(expression.representation)
+        start = apply_indexing(
+            values_needed["coordinate_system_display"], values_needed["start"]
         )
-        start = apply_indexing(coordinate_system_display, start_value)
-        start_pos = self._decimal_to_int_strict(start)
-        end_pos = self._decimal_to_int_strict(end_value)
-        alt_sequence = self._validate_vrs_sequence_string(allele_state_literal)
+        start_pos = self._convert_decimal_to_int(start)
+        end_pos = self._convert_decimal_to_int(values_needed["end"])
+        alt_seq = self._validate_sequence(seq)
 
-        refget_accession = self.dp.derive_refget_accession(f"refseq:{refseq_id}")
-        vrs_sequence_reference = SequenceReference(
+        refget_accession = self.dp.derive_refget_accession(
+            f"refseq:{values_needed['refseq']}"
+        )
+        seq_ref = SequenceReference(
             refgetAccession=refget_accession.split("refget:")[-1]
         )
 
-        vrs_sequence_location = SequenceLocation(
-            sequenceReference=vrs_sequence_reference,
+        seq_location = SequenceLocation(
+            sequenceReference=seq_ref,
             start=start_pos,
             end=end_pos,
         )
-        literal_sequence_expression = LiteralSequenceExpression(
-            sequence=sequenceString(alt_sequence)
-        )
-        allele = VrsAllele(
-            location=vrs_sequence_location, state=literal_sequence_expression
-        )
+        lit_seq_expr = LiteralSequenceExpression(sequence=sequenceString(alt_seq))
+        allele = Allele(location=seq_location, state=lit_seq_expr)
 
         return self.service.normalize(allele) if normalize else allele
 
     def translate_allele_to_fhir(self, expression):
+        """Converts an GA4GH VRS Allele object into FHIR Allele object.
+
+        Args:
+            expression (Allele): A VRS Allele object containing refgetAccession, start and end position, and alternative sequence.
+
+        Raises:
+            InvalidVRSAlleleError: Raised if the input is not a valid VRS Allele object.
+
+        Returns:
+            FhirAllele: The translated FHIR Allele Profile representation.
+        """
+
+        validate_vrs_allele(expression)
+
         if expression.state.type == "ReferenceLengthExpression":
             expression = self.allele_denormalizer.denormalize_reference_length(
                 expression
             )
 
-        refseq_id, start_pos, end_pos, alt_allele = (
-            self._extract_vrs_allele_translation_core(expression, self.dp)
+        refgetAccession, start_pos, end_pos, alt_allele = self._extract_vrs_values(
+            expression, self.dp
         )
 
-        # Build Sequence Profile
-        sequence_identifier = CodeableConcept(
-            coding=[
-                Coding(
-                    system="http://www.ncbi.nlm.nih.gov/refseq",
-                    code=refseq_id,
-                    # display="TBD-THIS IS A DEMO EXAMPLE"
-                )
-            ]
-        )
+        sequence_type = detect_sequence_type(refgetAccession)
 
-        representation_sequence = MolecularDefinitionRepresentation(
-            code=[sequence_identifier]
-        )
-
-        sequence_type = detect_sequence_type(refseq_id)
-
-        molecule_type = CodeableConcept(
+        mol_type = CodeableConcept(
             coding=[
                 Coding(
                     system="http://hl7.org/fhir/sequence-type",
@@ -308,17 +302,21 @@ class VrsFhirAlleleTranslator:
             ]
         )
 
-        fhir_id = refseq_to_fhir_id(refseq_accession=refseq_id)
-
-        sequence_profile = FhirSequenceProfile(
-            id=f"ref-to-{fhir_id}",
-            moleculeType=molecule_type,
-            representation=[representation_sequence],
+        coding_ref = Coding(
+            system="http://www.ncbi.nlm.nih.gov/refseq",
+            code=refgetAccession,
+            # display="TBD-THIS IS A DEMO EXAMPLE"
         )
 
-        # Build Allele Location
-        sequence_context = Reference(
-            reference=f"#{sequence_profile.id}", type="MolecularDefinition"
+        code_value = CodeableConcept(coding=[coding_ref])
+        representation_sequence = MolecularDefinitionRepresentation(code=[code_value])
+
+        fhir_id = refseq_to_fhir_id(refseq_accession=refgetAccession)
+
+        sequence_profile = FhirSequence(
+            id=f"ref-to-{fhir_id}",
+            moleculeType=mol_type,
+            representation=[representation_sequence],
         )
 
         start_quant = Quantity(value=int(start_pos))
@@ -326,25 +324,9 @@ class VrsFhirAlleleTranslator:
 
         system, origin, normalizationMethod = vrs_coordinate_interval()
 
-        coord_system_fhir = MolecularDefinitionLocationSequenceLocationCoordinateIntervalCoordinateSystem(
-            system=system, origin=origin, normalizationMethod=normalizationMethod
+        seq_context = Reference(
+            reference=f"#{sequence_profile.id}", type="MolecularDefinition"
         )
-
-        coord_interval = MolecularDefinitionLocationSequenceLocationCoordinateInterval(
-            coordinateSystem=coord_system_fhir,
-            startQuantity=start_quant,
-            endQuantity=end_quant,
-        )
-
-        sequence_location = MolecularDefinitionLocationSequenceLocation(
-            sequenceContext=sequence_context, coordinateInterval=coord_interval
-        )
-
-        location = MolecularDefinitionLocation(sequenceLocation=sequence_location)
-
-        # Build Allele Representation
-        allele_literal = MolecularDefinitionRepresentationLiteral(value=str(alt_allele))
-
         focus_value = CodeableConcept(
             coding=[
                 Coding(
@@ -355,13 +337,30 @@ class VrsFhirAlleleTranslator:
             ]
         )
 
-        allele_representation = MolecularDefinitionRepresentation(
-            focus=focus_value, literal=allele_literal
+        moldef_literal = MolecularDefinitionRepresentationLiteral(value=str(alt_allele))
+
+        moldef_repr = MolecularDefinitionRepresentation(
+            focus=focus_value, literal=moldef_literal
         )
 
-        return FhirAlleleProfile(
+        coord_system_fhir = MolecularDefinitionLocationSequenceLocationCoordinateIntervalCoordinateSystem(
+            system=system, origin=origin, normalizationMethod=normalizationMethod
+        )
+        coord_interval = MolecularDefinitionLocationSequenceLocationCoordinateInterval(
+            coordinateSystem=coord_system_fhir,
+            startQuantity=start_quant,
+            endQuantity=end_quant,
+        )
+
+        seq_location = MolecularDefinitionLocationSequenceLocation(
+            sequenceContext=seq_context, coordinateInterval=coord_interval
+        )
+
+        location = MolecularDefinitionLocation(sequenceLocation=seq_location)
+
+        return FhirAllele(
             contained=[sequence_profile],
-            moleculeType=molecule_type,
+            moleculeType=mol_type,
             location=[location],
-            representation=[allele_representation],
+            representation=[moldef_repr],
         )
